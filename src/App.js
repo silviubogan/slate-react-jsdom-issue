008ae260498e8e9dd1653e19b2226507aba08d14
Silviu Bogan <silviubogan@gmail.com>
2021-10-11 11:18:35 +0300

    Second commit, clearly shows the issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, withReact, Editable } from "slate-react";
 
-function App() {
+export default function App() {
+  const editor = useMemo(() => {
+    return withReact(createEditor());
+  }, []);
+  const [value, setValue] = useState([
+    {
+      type: "p",
+      children: [{ text: "simple text" }]
+    }
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <Slate
+        editor={editor}
+        value={value}
+        onChange={(v) => {
+          setValue(v);
+        }}
+      >
+        <Editable />
+      </Slate>
     </div>
   );
 }
-
-export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import renderer from "react-test-renderer";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("my test", () => {
+  const component = renderer.create(<App></App>);
+  const json = component.toJSON();
+  expect(json).toMatchSnapshot();
 });
